4a49ca5e2beac1fa6bb57b3d733a3afba5866dfc
dtanner <dan@dantanner.com>
2023-08-01 00:39:41 -0500

    test

--- check-permissions.sh
+++ check-permissions.sh
@@ -11,23 +11,29 @@ GITHUB_TOKEN=$1
 
 # Make the API request
 RESPONSE=$(curl -s -H "Authorization: token $GITHUB_TOKEN" -H "Accept: application/vnd.github.v3+json" "https://api.github.com/repos/$GITHUB_REPOSITORY")
+echo "response for matching repo:"
+echo $RESPONSE
+
+RESPONSE=$(curl -s -H "Authorization: token $GITHUB_TOKEN" -H "Accept: application/vnd.github.v3+json" "https://api.github.com/repos/dtanner/chaperone")
+echo "response for non-matching repo"
+echo $RESPONSE
 
 # Extract permissions from the response
-PERMISSIONS=$(echo $RESPONSE | jq -r '.permissions')
-echo "$PERMISSIONS"
+#PERMISSIONS=$(echo $RESPONSE | jq -r '.permissions')
+#echo "$PERMISSIONS"
 # Extract admin and push permissions from the response
-ADMIN_PERMISSION=$(echo $RESPONSE | jq -r '.permissions.admin')
-PUSH_PERMISSION=$(echo $RESPONSE | jq -r '.permissions.push')
+#ADMIN_PERMISSION=$(echo $RESPONSE | jq -r '.permissions.admin')
+#PUSH_PERMISSION=$(echo $RESPONSE | jq -r '.permissions.push')
 
 # Check permissions
-if [[ $ADMIN_PERMISSION == "true" ]] || [[ $PUSH_PERMISSION == "true" ]]; then
-  echo "This token has write permissions to the repository."
-else
-  echo "This token does not have write permissions to the repository."
-fi
+#if [[ $ADMIN_PERMISSION == "true" ]] || [[ $PUSH_PERMISSION == "true" ]]; then
+#  echo "This token has write permissions to the repository."
+#else
+#  echo "This token does not have write permissions to the repository."
+#fi
 
-echo "api.github.com call:"
-curl -sS -f -I -H "Authorization: token $GITHUB_TOKEN" https://api.github.com
+#echo "api.github.com call:"
+#curl -sS -f -I -H "Authorization: token $GITHUB_TOKEN" https://api.github.com
 
-echo "api.github.com/dtanner/junk call:"
-curl -sS -f -I -H "Authorization: token $GITHUB_TOKEN" https://api.github.com/dtanner/junk
+#echo "api.github.com/dtanner/junk call:"
+#curl -sS -f -I -H "Authorization: token $GITHUB_TOKEN" https://api.github.com/dtanner/junk
